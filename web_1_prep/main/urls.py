4228f4bb352255e5621f08e14f35a8f6b962441e
Batsandi <geol.aleksandar@gmail.com>
2022-02-26 11:04:26 +0200

    configured static files css for home no profile template

--- web_1_prep/main/urls.py
+++ web_1_prep/main/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+
+from web_1_prep.main.views.generic import show_home
+
+urlpatterns = [
+    path('', show_home, name='index')
+]
--- web_1_prep/settings.py
+++ web_1_prep/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-3n@g2g761bfgy1l2yt=^#a%t^29x98izevsry*#)$zkh%of3iw
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -125,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
